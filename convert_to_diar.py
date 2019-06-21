1ed3f54db6b17a1965eda82dea374470ced5db9b
Chuck Cho <ccho@axon.com>
2019-06-21 16:28:22 -0700

    Now supports Google STT output

--- convert_to_diar.py
+++ convert_to_diar.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
 """
-Converts Azure STT output to a diarization file (which will be eventually
+Converts Azure/Google STT output to a diarization file (which will be eventually
 consumed by DER calculation script)
+This will automatically detect which STT service produced the transcript json
+file.
 
-Usage: python3 convert_azure_to_diar.py input_azure_out.json output_diar.json
+Usage: python3 convert_to_diar.py input_out.json output_diar.json
 """
 
 import json
 import logging
 import sys
 logging.basicConfig(level=logging.DEBUG)
+
+def parse_stt_results(results):
+    if "AudioFileResults" in results:
+        logging.info("Azure transcript detected.")
+        return parse_azure_stt_results(results)
+    elif "results" in results:
+        logging.info("Google transcript detected.")
+        return parse_google_stt_results(results)
+    else:
+        logging.error("Currently supported STT services: Azure or Google. " \
+                "Panic!")
+        sys.exit(-3)
 
 def parse_azure_stt_results(results, ignore_null=True):
     """ Parse Azure STT result json file, and optionally ignore null as an
@@ -23,10 +37,38 @@ def parse_azure_stt_results(results, ignore_null=True):
         sid = seg["SpeakerId"]
         if ignore_null and sid == None:
             continue
-        offset = seg["Offset"] / 1.e6
-        end = offset + seg["Duration"] / 1.e6
+        offset = seg["Offset"] / 1.e7
+        end = offset + seg["Duration"] / 1.e7
         diar_seg = (sid, offset, end)
         diar.append(diar_seg)
+    return diar
+
+def parse_google_stt_results(results):
+    """ Parse Google Cloud STT result json file
+    """
+    diar = []
+    for res in results["results"]:
+        assert "alternatives" in res
+        alts = res["alternatives"]
+        assert len(alts) == 1
+        assert "words" in alts[0]
+        assert len(alts[0]["words"]) >= 1
+        for word in alts[0]["words"]:
+            if "speakerTag" not in word:
+                continue
+
+            start_time = word["startTime"]
+            assert start_time.endswith("s")
+            start_time = float(start_time[:-1])
+
+            end_time = word["endTime"]
+            assert end_time.endswith("s")
+            end_time = float(end_time[:-1])
+
+            speaker_tag = str(word["speakerTag"])
+
+            diar_seg = (speaker_tag, start_time, end_time)
+            diar.append(diar_seg)
     return diar
 
 def sort_diar(diar):
@@ -50,14 +92,13 @@ def check_overlap(diar):
                     "Panic!".format(d1[1], d1[2], d2[1], d2[2]))
     return
 
-def round_diar(diar, dec_pt=1):
+def round_diar(diar, dec_pt=2):
     """ Round diarization times at the given decimal point
     """
     diar2 = []
     for d in diar:
         diar2.append((d[0], round(d[1], dec_pt), round(d[2], dec_pt)))
     return diar2
-
 
 def convert(argv):
     if len(argv) != 3:
@@ -69,17 +110,18 @@ def convert(argv):
         logging.error("Run-time error={} while loading an input file={}".format(
                 inst, argv[1]))
 
-    diar = parse_azure_stt_results(tr)
+    # Extract speaker IDs and time intervals
+    diar = parse_stt_results(tr)
 
     # Make sure diarization results are sorted by the offset
     diar = sort_diar(diar)
 
+    # Remove long decimal points
+    diar = round_diar(diar)
+
     # SimpleDER hates, oops, ignores overlaps. Better to check beforehand.
     # Azure probably doesn't support overlapping diarization anyway.
     check_overlap(diar)
-
-    # Remove long decimal points
-    diar = round_diar(diar)
 
     try:
         with open(argv[2], 'w') as fout:
